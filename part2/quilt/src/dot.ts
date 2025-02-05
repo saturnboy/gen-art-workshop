f6a74f05fc67021a7d4f93e9810b5d58725d6e2c
Justin Shacklette <justin@saturnboy.com>
2025-02-04 22:55:35 -0700

    dot.ts is mostly transparent rounded rect

--- part2/quilt/src/dot.ts
+++ part2/quilt/src/dot.ts
@@ -4,6 +4,6 @@ export const patch: Patch = function (buf) {
     const sz = buf.width;
 
     buf.noStroke();
-    buf.fill(0);
-    buf.circle(sz * 0.5, sz * 0.5, 5);
+    buf.fill(0, 32);
+    buf.rect(sz * 0.5 - 8, sz * 0.5 - 8, 8, 8, 2);
 };
