f687f92d20c03428793e8aa82cb5830157097c7d
Justin Shacklette <justin@saturnboy.com>
2025-02-12 10:10:33 -0700

    add playwright browser screenshot capture

---
+++ part2/capture/src/app.ts
@@ -0,0 +1,18 @@
+import P5 from "p5";
+
+const sketch = (p5: P5) => {
+    p5.windowResized = () => {
+        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    };
+
+    p5.setup = () => {
+        p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        p5.noLoop();
+    };
+
+    p5.draw = () => {
+        p5.background(255);
+    };
+};
+
+new P5(sketch);
