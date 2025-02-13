bd06e09c061187864f995d1ccc80cb90a58d8351
Justin Shacklette <justin@saturnboy.com>
2025-02-12 18:30:35 -0700

    capture test, part 2

--- part2/capture/tests/screenshot.spec.ts
+++ part2/capture/tests/screenshot.spec.ts
@@ -4,7 +4,7 @@ test("screenshot", async ({ page }) => {
     await page.goto("/");
     for (let i = 1; i <= 3; i++) {
         // capture
-        await page.screenshot({ path: `out-${i}.png` });
+        await page.screenshot({ path: `quilt-${i}.png` });
 
         // redraw (by sending space)
         await page.keyboard.type(" ");
