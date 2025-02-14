fee1c1c3b65f17aece1c39a805b698932f0db80d
Jake <jfakult15@gmail.com>
2025-02-13 18:29:37 -0700

    Add squiggly lines

--- part2/quilt/src/ex_j.ts
+++ part2/quilt/src/ex_j.ts
@@ -1,7 +1,48 @@
 import type { Patch } from "./app";
+import P5 from "p5";
 
 export const patch: Patch = function (buf) {
-    const rows = buf.random([2, 3, 4, 5]),
+    // Draw a squiqqly s shape
+    const s_thickness = buf.random(1, 10),
+        s_offset = buf.random(-s_thickness, s_thickness),
+        s_angle = buf.random(-1, 1)
+    
+    const w = buf.width / 2,
+        h = buf.height / 2
+
+    drawSquiggle(buf, w, h, 0.2);
+};
+
+// Adapted from: https://editor.p5js.org/cdaein/sketches/SkQmYIRqX
+function drawSquiggle(buf : P5.Graphics, w: number, h: number, scale: number) {
+    // draw squiggle
+    buf.strokeWeight(3);
+    buf.stroke(0);
+    buf.noFill();
+    buf.beginShape();
+
+    const randomX = buf.random(w * scale),
+        randomY = buf.random(h * scale);
+
+    const x = w * scale,
+        y = h * scale;
+    buf.curveVertex(x, y);
+
+    for (let xx = x; xx < w; xx += 25) {
+        let yy = buf.random(-h, h);
+        buf.curveVertex(xx, yy + randomY);
+        buf.curveVertex(xx + randomX, yy + randomY);
+        buf.curveVertex(xx + randomX, yy + randomY);
+        buf.curveVertex(xx + randomX, yy + randomY);
+        buf.curveVertex(xx + randomX, yy + randomY);
+        buf.curveVertex(xx + randomX, yy + randomY);
+        buf.curveVertex(xx + randomX, yy + randomY);
+    }
+    buf.endShape();
+}
+
+/*
+const rows = buf.random([2, 3, 4, 5]),
         cols = rows,
         s = buf.width / rows;
 
@@ -22,4 +63,4 @@ export const patch: Patch = function (buf) {
             }
         }
     }
-};
+*/
