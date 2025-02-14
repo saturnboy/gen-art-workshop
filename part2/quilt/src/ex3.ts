cc7661a0e518565bf90c157aabaf747b67765103
Phu Le <phle0007@colorado.edu>
2025-02-13 18:27:21 -0700

    Update ex3.ts

--- part2/quilt/src/ex3.ts
+++ part2/quilt/src/ex3.ts
@@ -2,7 +2,7 @@ import type { Patch } from "./app";
 
 export const patch: Patch = function (buf) {
     const sz = buf.width,
-        steps = [-3, -2, -1, 0, 0, 0, 1, 2, 3];
+        steps = [-20, -10, -1, 0, 0, 0, 1, 10, 20];
 
     const sq = (x: number, y: number, sz: number) => {
         if (sz <= 5) {
@@ -22,7 +22,12 @@ export const patch: Patch = function (buf) {
         buf.vertex(x + d4, y + sz + d4);
         buf.endShape(buf.CLOSE);
 
-        const r = buf.random([1, 2, 3, 4, 5, 6, 7, 8]);
+
+        //const r = buf.random([1, 2, 3, 4, 5, 6, 7, 8]);
+        
+        let r = buf.floor(buf.random(1, 100));
+        r = Math.ceil(Math.abs(Math.log(Math.cos(r) * Math.cos(r))));
+
         sq(x + r, y + r, sz - r * 2);
     };
 
