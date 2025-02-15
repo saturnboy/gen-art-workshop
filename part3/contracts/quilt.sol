35a0e67a7005bbe0bcacb135d88f51ec3b4d6af0
Justin Shacklette <justin@saturnboy.com>
2025-02-15 12:35:12 -0700

    fixup contract, first token is #1

--- part3/contracts/quilt.sol
+++ part3/contracts/quilt.sol
@@ -8,15 +8,17 @@ contract Quilt is ERC721, Ownable {
     uint256 private _nextTokenId;
     uint256 private constant MAX_SUPPLY = 3;
 
-    constructor(address initialOwner) ERC721("Quilt", "QLT") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC721("Quilt", "QLT") Ownable(initialOwner) {
+        _nextTokenId = 1; // first token is #1
+    }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://asQmbCtH398mJZEHsGLhU8BQ73kqK1bcYt9anKtcUGQiS1Gzdf/";
+        return "ipfs://QmbCtH398mJZEHsGLhU8BQ73kqK1bcYt9anKtcUGQiS1Gz/";
     }
 
     function safeMint(address to) public onlyOwner {
+        require(_nextTokenId <= MAX_SUPPLY, "exceeded max supply");
         uint256 tokenId = _nextTokenId++;
-        require(tokenId <= MAX_SUPPLY, "exceeded max supply");
         _safeMint(to, tokenId);
     }
 }
